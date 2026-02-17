dc6ae3fb88f65d0dd774c6d02aab040a4ca48d8a
MattechIT <matteo.todeschi2@studio.unibo.it>
2026-02-17 17:41:21 +0100

    Modified config parameters

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 // Shared configuration (geometric values, speed, lenghts etc.)
 export const NODE_RADIUS = 20;
-export const PLAYBACK_SPEED = 2000; // ms
+export const PLAYBACK_SPEED = 1500; // ms
 export const THROTTLE_DELAY = 100; // ms
 export const DEFAULT_PADDING = 80;
-export const LABEL_MAX_LENGTH = 2;
+export const LABEL_MAX_LENGTH = 5;
